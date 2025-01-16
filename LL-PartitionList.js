348344ae611e2a4c975f20ba8b972a95a634ac01
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2025-01-15 20:09:20 -0500

    completed linked list partition list exercise

---
+++ LL-PartitionList.js
@@ -0,0 +1,29 @@
+function partitionList(x) {
+    if(length <= 1) {
+        return
+    }
+    
+    let front = new Node(0);
+    let back = new Node(0);
+    
+    let frontPointer = front;
+    let backPointer = back;
+    
+    let current = this.head;
+    
+    while(current) {
+        if(current.value < x) {
+            frontPointer.next = current;
+            frontPointer = frontPointer.next;
+        } else {
+            backPointer.next = current;
+            backPointer = backPointer.next;
+        }
+        
+        current = current.next;
+    }
+    
+    backPointer.next = null;
+    frontPointer.next = back.next;
+    this.head = front.next;
+}
