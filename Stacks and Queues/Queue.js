3c261bfa13f019f3fcae5716a7f920b5e9f28932
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2025-02-13 10:30:43 -0500

    completed queue enqueue method

--- Stacks and Queues/Queue.js
+++ Stacks and Queues/Queue.js
@@ -12,4 +12,19 @@ class Queue {
         this.last = newNode;
         this.length = 1;
     }
+
+    enqueue(value) {
+        const newNode = new Node(value);
+
+        if(this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
 }
