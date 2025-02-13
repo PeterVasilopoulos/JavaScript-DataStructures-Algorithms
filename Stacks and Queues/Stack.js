e6b87fedccb66b195f1d648aff46c10a165beac9
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2025-02-13 10:05:53 -0500

    completed stack constructor

---
+++ Stacks and Queues/Stack.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.top = newNode;
+        //this.bottom = newNode;
+        this.length = 1;
+    }
+}
